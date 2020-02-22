cad08d569da0db75336e36e8d5d0543244eef204
Hüsen Baghayi <hossein.baghayi@gmail.com>
2020-02-22 16:16:15 +0330

    recipient name

--- src/Recipient.php
+++ src/Recipient.php
@@ -9,9 +9,11 @@ use Baghayi\Value\Email;
 class Recipient
 {
     public $email;
+    public $name;
 
-    public function __construct(Email $email)
+    public function __construct(Email $email, string $name)
     {
         $this->email = $email;
+        $this->name = $name;
     }
 }
--- test/RecipientTest.php
+++ test/RecipientTest.php
@@ -25,8 +25,17 @@ class RecipientTest extends TestCase
     */
     public function recipient_email()
     {
-        $recipient = new Recipient(new Email(self::EMAIL));
+        $recipient = new Recipient(new Email(self::EMAIL), '');
         $this->assertInstanceof(Email::class, $recipient->email);
         $this->assertSame(self::EMAIL, (string) $recipient->email);
+    }
+
+    /**
+    * @test
+    */
+    public function recipient_name()
+    {
+        $recipient = new Recipient(new Email(self::EMAIL), 'shahdana');
+        $this->assertSame('shahdana', $recipient->name);
     }
 }
