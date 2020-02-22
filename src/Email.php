ff4afa61f0de806ca3f20fe0e597d7af1888707d
Hüsen Baghayi <hossein.baghayi@gmail.com>
2020-02-22 16:22:53 +0330

    email recipient

--- src/Email.php
+++ src/Email.php
@@ -8,10 +8,12 @@ class Email
 {
     public $subject;
     public $message;
+    public $recipient;
 
-    public function __construct(string $subject, string $message)
+    public function __construct(string $subject, string $message, Recipient $recipient)
     {
         $this->subject = $subject;
         $this->message = $message;
+        $this->recipient = $recipient;
     }
 }
--- test/EmailTest.php
+++ test/EmailTest.php
@@ -5,12 +5,21 @@ declare(strict_types=1);
 namespace Test\Evand\Email;
 
 use Evand\Email\Email;
+use Baghayi\Value\Email as EmailAddress;
+use Evand\Email\Recipient;
 use PHPUnit\Framework\TestCase;
 
 class EmailTest extends TestCase
 {
     const SUBJECT = 'good bye party Jafare';
     const MESSAGE = 'email body';
+    const EMAIL = 'husen@google.com';
+    private $recipient;
+
+    public function setUp(): void
+    {
+        $this->recipient = new Recipient(new EmailAddress(self::EMAIL), 'husen');
+    }
 
     /**
     * @test
@@ -25,7 +34,7 @@ class EmailTest extends TestCase
     */
     public function email_subject()
     {
-        $email = new Email(self::SUBJECT, '');
+        $email = new Email(self::SUBJECT, '', $this->recipient);
         $this->assertSame(self::SUBJECT, $email->subject);
     }
 
@@ -34,7 +43,17 @@ class EmailTest extends TestCase
     */
     public function email_message()
     {
-        $email = new Email(self::SUBJECT, self::MESSAGE);
+        $email = new Email(self::SUBJECT, self::MESSAGE, $this->recipient);
         $this->assertSame(self::MESSAGE, $email->message);
+    }
+
+    /**
+    * @test
+    */
+    public function email_recipient()
+    {
+        $recipient = new Recipient(new EmailAddress(self::EMAIL), 'husen');
+        $email = new Email('', '', $recipient);
+        $this->assertSame($recipient, $email->recipient);
     }
 }
