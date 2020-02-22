22a5861c012daeeaeb0d550d1233ee40f6675901
Hüsen Baghayi <hossein.baghayi@gmail.com>
2020-02-22 16:14:44 +0330

    recipient email

--- src/Recipient.php
+++ src/Recipient.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace Evand\Email;
 
+use Baghayi\Value\Email;
+
 class Recipient
 {
+    public $email;
+
+    public function __construct(Email $email)
+    {
+        $this->email = $email;
+    }
 }
--- test/RecipientTest.php
+++ test/RecipientTest.php
@@ -4,16 +4,29 @@ declare(strict_types=1);
 
 namespace Test\Evand\Email;
 
+use Baghayi\Value\Email;
 use Evand\Email\Recipient;
 use PHPUnit\Framework\TestCase;
 
 class RecipientTest extends TestCase
 {
+    const EMAIL = 'husen@gmail.com';
+
     /**
     * @test
     */
     public function recipient()
     {
         $this->assertTrue(class_exists(Recipient::class));
+    }
+
+    /**
+    * @test
+    */
+    public function recipient_email()
+    {
+        $recipient = new Recipient(new Email(self::EMAIL));
+        $this->assertInstanceof(Email::class, $recipient->email);
+        $this->assertSame(self::EMAIL, (string) $recipient->email);
     }
 }
