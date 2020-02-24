0730cc5317b497e9bb82c7d1972590beeb621594
Hüsen Baghayi <hossein.baghayi@gmail.com>
2020-02-24 15:16:43 +0330

    send email

--- src/EmailSender/Sender.php
+++ src/EmailSender/Sender.php
@@ -4,6 +4,31 @@ declare(strict_types=1);
 
 namespace Evand\Email\EmailSender;
 
+use Evand\Email\Email;
+use GuzzleHttp\Client;
+
 class Sender
 {
+    const RESOURCE_URI = 'https://kaftar.evand.com/api/send-email';
+
+    private $http;
+
+    public function __construct(Client $http)
+    {
+        $this->http = $http;
+    }
+
+    public function send(Email $email)
+    {
+        $this->http->request('POST', self::RESOURCE_URI, [
+            'json' => [
+                'subject' => $email->subject,
+                'message' => $email->message,
+                'recipient' => [
+                    'name' => $email->recipient->name,
+                    'email' => (string) $email->recipient->email,
+                ],
+            ]
+        ]);
+    }
 }
--- test/EmailSender/SenderTest.php
+++ test/EmailSender/SenderTest.php
@@ -4,16 +4,61 @@ declare(strict_types=1);
 
 namespace Test\Evand\Email\EmailSender;
 
+use Baghayi\Value\Email as EmailAddress;
+use Evand\Email\Email;
 use Evand\Email\EmailSender\Sender;
+use Evand\Email\Recipient;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 
 class SenderTest extends TestCase
 {
+    const RECIPIENT_EMAIL = 'not_husen_and_hamidreza@evand.com';
+    const RECIPIENT_NAME = 'husen';
+    const SUBJECT = 'f**k bye';
+    const MSG = 'Husen\'s last days in evand.';
+    const RESOURCE_URI = 'https://kaftar.evand.com/api/send-email';
+
     /**
     * @test
     */
     public function email_sender()
     {
         $this->assertTrue(class_exists(Sender::class));
+    }
+
+    /**
+    * @test
+    */
+    public function send_an_email()
+    {
+        $container = [];
+        $client = $this->configureGuzzle($container);
+        $sender = new Sender($client);
+        $sender->send(new Email(self::SUBJECT, self::MSG, new Recipient(new EmailAddress(self::RECIPIENT_EMAIL), self::RECIPIENT_NAME)));
+
+        $this->assertNotEmpty($container);
+        $this->assertSame('POST', $container[0]['request']->getMethod());
+        $this->assertSame(self::RESOURCE_URI, (string) $container[0]['request']->getUri());
+        $request_body = json_decode((string) $container[0]['request']->getBody());
+        $this->assertNotEmpty($request_body);
+        $this->assertSame(self::SUBJECT, $request_body->subject);
+        $this->assertSame(self::MSG, $request_body->message);
+        $this->assertSame(self::RECIPIENT_NAME, $request_body->recipient->name);
+        $this->assertSame(self::RECIPIENT_EMAIL, $request_body->recipient->email);
+    }
+
+    private function configureGuzzle(array &$container): Client
+    {
+        $history = Middleware::history($container);
+        $mock = new MockHandler([ new Response(200) ]);
+        $handlerStack = HandlerStack::create($mock);
+        $handlerStack->push($history);
+        $client = new Client(['handler' => $handlerStack]);
+        return $client;
     }
 }
